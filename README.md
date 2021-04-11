1a1498761ca00c598b260aad9f022527f2581c97
Yuliya Rubtsova <yuliya.rubcova89@mail.ru>
2021-04-11 17:30:18 +0800

    Changed AlfaCardTest

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/9rl03phrih08ndrm?svg=true)](https://ci.appveyor.com/project/Yuliyarubtsova/qa2-1-selenium)
---
+++ src/test/java/ru/netology/AlfaCardTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class AlfaCardTest {
+
+    @Test
+    void shouldTest1() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Василий");
+        $("[data-test-id=phone] input").setValue("+79168689672");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
